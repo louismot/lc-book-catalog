2a95c33f2095dd5fb1a123e4c46366bb34a462a4
louismot <louismot@buffalo.edu>
2020-09-08 17:30:43 -0400

    created table that populates from axios.get call (using mockapi for testing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Table v-if="tableData" :theData = "tableData" :config="config"/>
+
   </div>
 </template>
 
 <script>
-
+ import Table from "./components/Table";
 
 export default {
   name: 'App',
   components: {
-
+    Table
+  },
+  data: () => ({
+    tableData: undefined,
+    config:[
+      {
+        key: 'Level',
+        title: 'Level'
+      },
+      {
+        key: 'Title',
+        title: 'Title'
+      },
+      {
+        key: 'Author',
+        title: 'Author'
+      },
+      {
+        key: 'Available',
+        title: 'Available'
+      }
+    ]
+  }),
+  mounted () {
+    this.$axios.get('https://5f57dab31a07d600167e765c.mockapi.io/test/books')
+    .then(({data}) => {
+      this.tableData = data
+    })
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="book-table">
+    <table class="table">
+      <thead>
+
+      <tr>
+        <!--populate headers for each column in the table-->
+        <th v-for="(obj, ind) in config" :key="ind">{{ obj.title }}</th>
+      </tr>
+
+      </thead>
+      <tbody>
+      <!--loop through each row, and populate the correct data-->
+      <tr v-for="(row, index) in theData" :key="index">
+        <td v-for="(obj, ind) in config" :key="ind">{{ row[obj.key]}}</td>
+      </tr>
+
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  props: ['theData', 'config']
+}
+</script>
+
+<style scoped>
+
+</style>
